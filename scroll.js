95b98ac6832b47eb357165680340b7a1e879672f
cheesidibbl <zjz3060@students.mpsaz.org>
2024-11-02 08:54:36 -0700

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -9,3 +9,14 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden'); // Use querySelectorAll
 hiddenElements.forEach((el) => observer.observe(el)); // Iterate over the NodeList
+
+const scrollDown = () => {
+    const scrollPosition = window.scrollY;
+    if (scrollPosition > 200) { // Adjust the scroll position value as needed
+        document.querySelector('.sideslide').classList.add('show-sideslide');
+    } else {
+        document.querySelector('.sideslide').classList.remove('show-sideslide');
+    }
+};
+
+window.addEventListener('scroll', scrollDown);
