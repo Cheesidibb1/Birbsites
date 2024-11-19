36a147e3035158c9b7bfe01e4d79f9612b65048d
cheesidibbl <zjz3060@students.mpsaz.org>
2024-11-19 15:09:43 -0700

    Update deals.js

---
+++ js/deals.js
@@ -0,0 +1,7 @@
+// Hide the iframe initially
+document.getElementById('iframeDeal').style.display = 'none';
+
+// Show the iframe after 30 seconds
+setTimeout(function() {
+  document.getElementById('iframeDeal').style.display = 'block';
+}, 10000);
